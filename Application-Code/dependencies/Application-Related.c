ba65252a0f6069e15015dd144ef54db76775cf16
OLiwier_Desktop <oliwier.jaworski@student.pxl.be>
2024-05-12 22:31:43 +0200

    argument parser is able to recognise basic letter combinations

--- Application-Code/dependencies/Application-Related.c
+++ Application-Code/dependencies/Application-Related.c
@@ -5,6 +5,46 @@ void ARG_Parser(int* argc, char* argv[])
     if(*argc ==1){
         printUsage(argv[0]);
     }
+    else{
+        for(int c_arg =0; c_arg < *argc; c_arg++){
+            if(argv[c_arg][0] == '-'){
+               char arg_option = (argv[c_arg][1] == '-') ? option_rewrite(&argv[c_arg][2]) : argv[c_arg][1];
+               switch (arg_option)
+               {
+               case 'm':
+               printf("arg :m\n");
+                break;
+               case 'w':
+               printf("arg :w\n");
+                break;
+               case 'h':
+               printf("arg :h\n");
+                break;
+               case 'c':
+               printf("arg :c\n");
+                break;
+               case 'x':
+               printf("arg :x\n");
+                break;
+               default:
+                break;
+               }
+               } 
+        }
+    }
+}
+
+char option_rewrite(char * option){
+    if( strcasecmp( "money", option ) == 0 )
+        return 'm';
+    if( strcasecmp( "max-weight", option ) == 0 )
+        return 'w';
+    if( strcasecmp( "camp-file", option ) == 0 )
+        return 'c';
+    if( strcasecmp( "help", option ) == 0 )
+        return 'h';
+    if( strcasecmp( "cancel ", option ) == 0 )
+        return 'x';
 }
 
 void printUsage( char *programName )
@@ -16,6 +56,6 @@ void printUsage( char *programName )
 	        "  -w --max-weight <number>          Maximum weight before becoming encumbered\n"
 	        "  -c --camp-file <filename.log>     Optional camp file for all discovered items during play that stay in camp\n"
 	        "  -h --help                         Print this help menu.\n"
-            "  -c --cancel                       exit the application\n",
+            "  -x --cancel                       exit the application\n",
 	        programName );
 }
--- Application-Code/main.c
+++ Application-Code/main.c
@@ -5,9 +5,6 @@
 #include "dependencies/Application-Related.h"
 
 int main(int argc, char *argv[]){
-    for(int c_arg=0; c_arg < argc; c_arg++){
-        //printf("argument :%d =%s\n",c_arg, argv[c_arg]);
-    }
     ARG_Parser(&argc, argv); 
     //FILE * fileptr = fopen("equipment.txt","w");          --DEBUG
     //fetch_url_data(DND_EQUIPMENT_URL, fileptr);           --DEBUG
