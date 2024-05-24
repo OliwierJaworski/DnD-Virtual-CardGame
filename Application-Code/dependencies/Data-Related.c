317a72887e14e2b1df003234144c24b6b06a7c7f
OLiwier_Desktop <oliwier.jaworski@student.pxl.be>
2024-05-24 21:24:40 +0200

    scanning for '{' in json file
    the json_parser function is able to point to each { in the char array

--- Application-Code/dependencies/Data-Related.c
+++ Application-Code/dependencies/Data-Related.c
@@ -33,19 +33,72 @@ size_t Json_Data_CB(void *data, size_t size, size_t nmemb, void *clientp)
   mem->response[mem->size] = 0;
   return realsize;
 }
-void Json_Parse(struct item_T *item){
-  if(item == NULL || item->item_url == NULL) {
-  throw_error("NO ITEM",NOT_FOUND);
+
+void Json_Parse(struct item_T *item, char* stream){
+  size_t CATCount=0;
+  char** category_ptr = (char**) calloc(CATCount,sizeof(char*));
+  char* bufferPTR=NULL;
+  category_ptr[CATCount] = strchr(stream,'{');
+  printf("size of char** : %zu",sizeof(category_ptr));
+  
+
+  while( (bufferPTR = strchr(category_ptr[CATCount]+1,'{')) != NULL) {
+      CATCount++;
+      category_ptr = realloc(category_ptr, (CATCount+1) * sizeof(char*));
+      category_ptr[CATCount] = bufferPTR;
   }
-  fetch_url_data(item->item_url);
+  printf("\n%s\n",category_ptr[2]);
+ 
+  /*
+
+ 
+  
+
+       for(int i; i< CATCount; i++){
+      
+  }
+      
+      category_ptr[CATCount] = calloc(1,sizeof(char*));
+      category_ptr[CATCount] = bufferPTR;
+
+*/
+
+
+/*
+  
+
+      while( strchr(categories_ptr[categories-1]+1,'{') != NULL ) {
+            printf("\nis working still111!!\n");
+            categories_ptr[0]= malloc(sizeof(char*));
+            categories_ptr[categories-1] = strchr(categories_ptr[categories-1]+1,'{');
+            categories++;
+            printf("\nis working still222!!\n");
+            categories_ptr = ((sizeof(categories_ptr) / sizeof(char*)) < categories-1)? realloc(categories_ptr, categories): categories_ptr;
+      }
+      for(int cat=0; cat< categories; cat++){
+            printf("%c\n",categories_ptr[cat]);
+      }*/
+}
+
+
+
+char* substr_to_Scope(int indent, char** destination){
+
+      
+}
+
+      
+
+
+  /*fetch_url_data(item->item_url);
   printf("\nresponse:%s\n",chunk.response);
 
   if(chunk.response != NULL){
   free(chunk.response);
   chunk.response= NULL;
   chunk.size=0;
-  }
-}
+  }*/
+
 void throw_error(char * err_msg, int err_code){
   perror(err_msg);
   exit(err_code);
--- Application-Code/dependencies/Data-Related.h
+++ Application-Code/dependencies/Data-Related.h
@@ -2,6 +2,8 @@
 #define Data_Related 
 #define DND_EQUIPMENT_URL "https://www.dnd5eapi.co/api/equipment/"
 #include <curl/curl.h>
+#include <string.h>
+#include <stdlib.h>
 #include "Player-Related.h"
 #define JSON_EXAMPLE "{\"desc\":[],\"special\":[],\"index\":\"shield\",\"name\":\"Shield\",\"equipment_category\":{\"index\":\"armor\",\"name\":\"Armor\",\"url\":\"/api/equipment-categories/armor\"},\"armor_category\":\"Shield\",\"armor_class\":{\"base\":2,\"dex_bonus\":false},\"str_minimum\":0,\"stealth_disadvantage\":false,\"weight\":6,\"cost\":{\"quantity\":10,\"unit\":\"gp\"},\"url\":\"/api/equipment/shield\",\"contents\":[],\"properties\":[]}"
 
@@ -20,7 +22,7 @@ extern struct memory {
 struct item_T{
   char* item_url;
   char* name;
-  char** item_details;
+  char*** item_details;
   int amount;
   struct item_T* next_item;
   struct item_T* previous_item;
@@ -34,8 +36,9 @@ struct Coin_T{
 
 //functions
 int fetch_url_data(char * url);
-void Json_Parse(struct item_T *item);
+void Json_Parse(struct item_T *item, char* stream);
 size_t Json_Data_CB(void *data, size_t size, size_t nmemb, void *clientp);
+char* substr_to_Scope(int indent, char** destination);
 void throw_error(char * err_msg, int err_code);
 
 #endif
